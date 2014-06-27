ef071b4fa9dcf1f74382de6c292b092d136d9fc7
minotauro <minotauro@minotauro-N53SM.(none)>
2014-06-27 18:55:23 +0200

    aggiunta datarate device

--- AQM_top_2.cc
+++ AQM_top_2.cc
@@ -52,12 +52,17 @@ main (int argc, char *argv[])
   p2p.SetDeviceAttribute ("DataRate", StringValue ("1Gbps"));
   p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
   NetDeviceContainer d0d1 = p2p.Install (n0n1);
+  
   // Canale router<-->nodo2
-  p2p.SetDeviceAttribute ("DataRate", StringValue ("1Mbps"));
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("7Mbps"));
   p2p.SetChannelAttribute ("Delay", StringValue ("10ms"));
   NetDeviceContainer d2d1 = p2p.Install (n2n1);
+  Ptr<NetDevice> dev_ptr = d2d1.Get(1);
+  // è l'interfaccia giusta? controllo a che nodo è associata
+  Ptr<Node> nodo_associato = dev_ptr->GetNode();
+  std::cout << "ID: " << nodo_associato->GetId() << std::endl;
+  dev_ptr->SetAttribute("DataRate", StringValue ("64kbps"));
   
-
   // ----------------- ASSEGNAZIONE INDIRIZZI --------------------------
   NS_LOG_INFO ("Assign IP Addresses.");
   Ipv4AddressHelper ipv4;
@@ -77,13 +82,13 @@ main (int argc, char *argv[])
   
   // Definizione porte
   uint16_t port_ftp = 20;         // FTP data
-  uint32_t file_dim = 10000000; // 10Mb 
+  //uint32_t file_dim = 10000000; // 10Mb 
   
   // Mittente FTP (server su nodo 0)
   OnOffHelper onoff_ftp1("ns3::TcpSocketFactory", Address(InetSocketAddress (i2i1.GetAddress (0), port_ftp)));
   onoff_ftp1.SetConstantRate (DataRate ("5Mbps"));
-  onoff_ftp1.SetAttribute("PacketSize", UintegerValue(4096));
-  onoff_ftp1.SetAttribute("MaxBytes", UintegerValue(file_dim)); // 10 Mb
+  onoff_ftp1.SetAttribute("PacketSize", UintegerValue(512));
+  //onoff_ftp1.SetAttribute("MaxBytes", UintegerValue(file_dim)); // 10 Mb
   onoff_ftp1.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
   onoff_ftp1.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
   ApplicationContainer ftp_server1 = onoff_ftp1.Install (c.Get (0));
@@ -98,8 +103,8 @@ main (int argc, char *argv[])
 
   // Mittente FTP (server su nodo 2)
   OnOffHelper onoff_ftp2("ns3::TcpSocketFactory", Address(InetSocketAddress (i0i1.GetAddress (0), port_ftp)));
-  onoff_ftp2.SetConstantRate (DataRate ("2Mbps"));
-  onoff_ftp2.SetAttribute("PacketSize", UintegerValue(256));
+  onoff_ftp2.SetConstantRate (DataRate ("5Mbps"));
+  onoff_ftp2.SetAttribute("PacketSize", UintegerValue(512));
   onoff_ftp2.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
   onoff_ftp2.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
   ApplicationContainer ftp_server2 = onoff_ftp2.Install (c.Get (2));
