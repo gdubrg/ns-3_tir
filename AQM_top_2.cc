6c8cde5f3165cd5f24259bcdf78824a984b877a3
minotauro <minotauro@minotauro-N53SM.(none)>
2014-08-24 10:59:32 +0200

    accorciata la durata della simulazione di AQM2 a 12 secondi

--- AQM_top_2.cc
+++ AQM_top_2.cc
@@ -93,13 +93,13 @@ main (int argc, char *argv[])
   onoff_ftp1.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
   ApplicationContainer ftp_server1 = onoff_ftp1.Install (c.Get (0));
   ftp_server1.Start(Seconds(0.0));
-  ftp_server1.Stop(Seconds(15.0)); 
+  ftp_server1.Stop(Seconds(10.0)); 
   
   // Destinatario FTP (client su nodo 2)
   PacketSinkHelper sink_ftp1 ("ns3::TcpSocketFactory", Address (InetSocketAddress (Ipv4Address::GetAny (), port_ftp)));
   ApplicationContainer ftp_client1 = sink_ftp1.Install (c.Get (2));
   ftp_client1.Start (Seconds (0.0));
-  ftp_client1.Stop (Seconds (18.0));
+  ftp_client1.Stop (Seconds (12.0));
 
   // Mittente FTP (server su nodo 2)
   OnOffHelper onoff_ftp2("ns3::TcpSocketFactory", Address(InetSocketAddress (i0i1.GetAddress (0), port_ftp2)));
@@ -108,14 +108,14 @@ main (int argc, char *argv[])
   onoff_ftp2.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
   onoff_ftp2.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
   ApplicationContainer ftp_server2 = onoff_ftp2.Install (c.Get (2));
-  ftp_server2.Start(Seconds(5.0));
-  ftp_server2.Stop(Seconds(15.0)); 
+  ftp_server2.Start(Seconds(3.0));
+  ftp_server2.Stop(Seconds(10.0)); 
   
   // Destinatario FTP (client su nodo 0)
   PacketSinkHelper sink_ftp2 ("ns3::TcpSocketFactory", Address (InetSocketAddress (Ipv4Address::GetAny (), port_ftp2)));
   ApplicationContainer ftp_client2 = sink_ftp2.Install (c.Get (0));
   ftp_client2.Start (Seconds (0.0));
-  ftp_client2.Stop (Seconds (18.0));
+  ftp_client2.Stop (Seconds (12.0));
 
   AsciiTraceHelper ascii;
   p2p.EnableAsciiAll (ascii.CreateFileStream ("AQM_top2.tr"));
